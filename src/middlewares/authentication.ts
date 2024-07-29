94036b4c2e3bb23af6dd785621196a14b14d8df8
mariaaenc <maria.correia@gmail.com>
2024-07-29 17:49:39 -0300

    feat: add partner authentication

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -3,8 +3,14 @@ import { parseResponseResult } from '../common/parseResponseResult'
 
 export function authentication (request: Request, response: Response, next: NextFunction): void {
   try {
-    const providedApiKey = request.headers['x-apikey']
-    const requiredApiKey = process.env.APIKEY
+    const hasPartnerAuthentication = response.locals['has-partner-auth']
+    const partnerApiKey = request.headers['x-partnerapikey']
+    let providedApiKey = request.headers['x-apikey']
+    let requiredApiKey = process.env.APIKEY
+    if (hasPartnerAuthentication && partnerApiKey) {
+      providedApiKey = partnerApiKey
+      requiredApiKey = process.env.PARTNER_APIKEY
+    }
     const apiKeyErrorMessage = 'Wrong or missing apikey'
     if (providedApiKey !== requiredApiKey) throw new Error(apiKeyErrorMessage)
     next()
